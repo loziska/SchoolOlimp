9d3bdf63c57732743addce1abf8203a4889cdd02
loziska <104343208+loziska@users.noreply.github.com>
2025-03-13 21:18:49 +0300

    Update plane.py

--- block1/plane.py
+++ block1/plane.py
@@ -1,51 +1,40 @@
-def main():
-    import sys
-    input = sys.stdin.read
-    data = input().split()
-    
-    n = int(data[0])
-    m = int(data[1])
-    
-    seats = []
-    index = 2
-    for _ in range(n):
-        row = data[index]
-        seats.append(list(row))
-        index += 1
-    
-    total = 0
-    for row in seats:
-        if row[0] == 'X' and row[5] == '.':
-            row[5] = 'X'
-            total += 1
-        elif row[0] == '.' and row[5] == 'X':
-            row[0] = 'X'
-            total += 1
-        if row[1] == 'X' and row[4] == '.':
-            row[4] = 'X'
-            total += 1
-        elif row[1] == '.' and row[4] == 'X':
-            row[1] = 'X'
-            total += 1
-        if row[2] == 'X' and row[3] == '.':
-            row[3] = 'X'
-            total += 1
-        elif row[2] == '.' and row[3] == 'X':
-            row[2] = 'X'
-            total += 1
-    
-    if total < m:
-        print("Impossible")
-        return
-    
-    for row in seats:
-        for i in range(6):
-            if row[i] == '.' and m > 0:
-                row[i] = 'X'
-                m -= 1
-    
-    for row in seats:
-        print(''.join(row))
+count_row = 8
+count_passengers = 15
+imp = False
 
-if __name__ == "__main__":
-    main()
+plane = [[0,0,0,0,1,0],
+       [0,1,0,0,1,1],
+       [0,0,1,1,0,1],
+       [0,1,0,0,0,0],
+       [1,1,0,0,0,0],
+       [1,0,0,0,1,0],
+       [0,0,0,1,1,0],
+       [0,0,0,0,1,0]]
+
+online_registred = sum([sum(row) for row in plane])
+free_space = count_row * 6 - online_registred
+
+if count_passengers + online_registred > 6 * count_row:
+    print("Impossible")
+    imp = True
+else:
+    for row in range(count_row):
+        for place in range(3):
+            if (plane[row][place] == 1 or plane[row][5-place] == 1) and plane[row][place] != plane[row][5-place]:
+                plane[row][place], plane[row][5-place] = 1, 1
+                count_passengers -= 1
+    if count_passengers % 2 == 1:
+        print('Impossible')
+        imp = True
+    else:
+        for row in range(count_row):
+            for place in range(3):
+                if count_passengers > 0:
+                    if plane[row][place] == 0 and plane[row][5-place] == 0:
+                        plane[row][place], plane[row][5-place] = 1, 1
+                        count_passengers -= 2
+
+if not imp:
+    for row in plane:
+        print(row)  
+                   
